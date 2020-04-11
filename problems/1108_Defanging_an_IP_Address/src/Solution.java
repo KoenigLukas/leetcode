972385a7efa39f2f7a6109f8cb980225d9d6277b
Lukas König <lukas.koenig02@gmail.com>
2020-04-11 22:46:09 +0200

    5 not working Test 8 fails

--- problems/1108_Defanging_an_IP_Address/src/Solution.java
+++ problems/1108_Defanging_an_IP_Address/src/Solution.java
@@ -1,2 +1,7 @@
 public class Solution {
+
+    public String defangIPaddr(String address) {
+        return address.replaceAll("\\.","\\[\\.\\]");
+    }
+
 }
---
+++ problems/5_Longest_Palindromic_Substring/src/Solution.java
@@ -0,0 +1,38 @@
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Test1: " + longestPalindrome("c") + "\t\tExpected: c");
+        System.out.println("Test2: " + longestPalindrome("cbbd") + "\t\tExpected: bb");
+        System.out.println("Test3: " + longestPalindrome("bacbad") + "\t\tExpected: b");
+        System.out.println("Test4: " + longestPalindrome("babad") + "\t\tExpected: bab");
+        System.out.println("Test5: " + longestPalindrome("ccbd") + "\t\tExpected: cc");
+        System.out.println("Test6: " + longestPalindrome("bdcbab") + "\t\tExpected: bab");
+        System.out.println("Test7: " + longestPalindrome("ljgeraiocacabgjrhgahgacbababaccaac") + "\t\tExpected: babab");
+        System.out.println("Test8: " + longestPalindrome("kyyrjtdplseovzwjkykrjwhxquwxsfsorjiumvxjhjmgeueafubtonhlerrgsgohfosqssmizcuqryqomsipovhhodpfyudtusjhonlqabhxfahfcjqxyckycstcqwxvicwkjeuboerkmjshfgiglceycmycadpnvoeaurqatesivajoqdilynbcihnidbizwkuaoegmytopzdmvvoewvhebqzskseeubnretjgnmyjwwgcooytfojeuzcuyhsznbcaiqpwcyusyyywqmmvqzvvceylnuwcbxybhqpvjumzomnabrjgcfaabqmiotlfojnyuolostmtacbwmwlqdfkbfikusuqtupdwdrjwqmuudbcvtpieiwteqbeyfyqejglmxofdjksqmzeugwvuniaxdrunyunnqpbnfbgqemvamaxuhjbyzqmhalrprhnindrkbopwbwsjeqrmyqipnqvjqzpjalqyfvaavyhytetllzupxjwozdfpmjhjlrnitnjgapzrakcqahaqetwllaaiadalmxgvpawqpgecojxfvcgxsbrldktufdrogkogbltcezflyctklpqrjymqzyzmtlssnavzcquytcskcnjzzrytsvawkavzboncxlhqfiofuohehaygxidxsofhmhzygklliovnwqbwwiiyarxtoihvjkdrzqsnmhdtdlpckuayhtfyirnhkrhbrwkdymjrjklonyggqnxhfvtkqxoicakzsxmgczpwhpkzcntkcwhkdkxvfnjbvjjoumczjyvdgkfukfuldolqnauvoyhoheoqvpwoisniv") + "\t\tExpected: qahaq");
+    }
+
+
+    public static String longestPalindrome(String s) {
+        int len = s.length();
+        if (len <= 1) return s;
+        String substring = "";
+        boolean flag = true;
+        for (int i = 0; i < len; i++) {
+            for (int j = len - 1; j > i; j--) {
+                flag = true;
+                for (int k = 0; k < (j - i); k++) {
+                    if (s.charAt(i + k) != s.charAt(j - k)) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) {
+                    if ((j - i) > substring.length()) substring = s.substring(i, j + 1);
+                }
+            }
+        }
+        if (substring.equals("")) substring = s.substring(0, 1);
+        return substring;
+    }
+
+}
